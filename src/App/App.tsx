191165b50736b8750afeb3dbd7ca347458e81346
DIXNEUF Baptiste <baptiste.dixneuf@gmail.com>
2022-04-04 14:54:55 +0200

    format code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
-import './App.css';
-import Button from './components/Button/Button';
-
+import React from "react";
+import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <div className="App">
       Hello world
-      <Button text="text du button" bgcolor="red" eventClicked={(arg : any) => {
-         console.log(arg);
-      }}/>
-      <Button text="text du button 2"/>
-      <Button text="text du button 3"/>
-      <Button text="text du button 3"/>
+      <Button
+        text="text du button"
+        bgcolor="red"
+        eventClicked={(arg: any) => {
+          console.log(arg);
+        }}
+      >
+        Hello
+      </Button>
+      <Button text="text du button 2">
+        <div>Hello</div>
+      </Button>
+      <Button text="text du button 3">
+        <div>Hello</div>
+        <div>Image</div>
+      </Button>
+      <Button bgcolor="blue" color="green" text="text du button 4">
+        {" "}
+      </Button>
     </div>
   );
 }
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,31 +1,41 @@
 import React from "react";
-import style from './Button.module.css';
-import PropTypes from 'prop-types';
+import style from "./Button.module.css";
+import PropTypes from "prop-types";
 
 function Button(props) {
-    console.log(props);
-    //const textAdded = props.text+'Hello'
-    return (
-        <button onClick={(evt) => {
-            props.eventClicked("Hello")
-        }} 
-        className={style.Button} 
-        style={ {backgroundColor:props.bgcolor}}>
-            {props.text}
-        </button>
-    );
+  console.log(props);
+  //const textAdded = props.text+'Hello'
+  return (
+    <button
+      onClick={(evt) => {
+        props.eventClicked("Hello");
+      }}
+      className={style.Button}
+      style={{ backgroundColor: props.bgcolor, color: props.color }}
+      type={props.type}
+    >
+      {props.text}
+      {props.children}
+    </button>
+  );
 }
 
-Button.propTypes={
-    text: PropTypes.string.isRequired,
-    eventClicked: PropTypes.func.isRequired,
-    bgcolor : PropTypes.string
-}
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  eventClicked: PropTypes.func.isRequired,
+  bgcolor: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+  type: PropTypes.string.isRequired,
+};
 
-Button.defaultProps={
-    eventClicked: () =>{
-        console.error('Function undefined')
-    }
-}
+Button.defaultProps = {
+  eventClicked: () => {
+    console.error("Function undefined");
+  },
+  bgcolor: "red",
+  color: "black",
+  type: "button",
+};
 
 export default Button;
